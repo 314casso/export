0ed31df3af9ad46d54e072773ed3e24be8b4b6a1
314casso <picasso75@yandex.ru>
2017-07-31 12:39:34 +0300

    fix

--- export/nutep/models.py
+++ export/nutep/models.py
@@ -16,11 +16,13 @@ from django.utils.formats import date_format
 def attachment_path(instance, filename):    
     from django.conf import settings
     os.umask(0)
-    path = u'attachments/%s_%s' % (datetime.date.today().month, datetime.date.today().year,)
+    path = u'attachments/%s_%s/%s' % (datetime.date.today().month, datetime.date.today().year,)
     att_path = os.path.join(settings.MEDIA_ROOT, path)
     if not os.path.exists(att_path):
         os.makedirs(att_path, 0777)
-    return os.path.join(path, force_text(filename))
+    return u'%s/%s' % (path, filename)
+#     return os.path.join(path, force_text(filename))
+
 
 
 class File(models.Model):
